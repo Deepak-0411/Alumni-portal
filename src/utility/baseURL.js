f70520bed610e6a2171268e920f5c2a3bb0624ec
Deepak-0411 <deepak0411.kr@gmail.com>
2025-12-14 19:40:23 +0530

    fix: base URL for backend API access

--- src/utility/baseURL.js
+++ src/utility/baseURL.js
@@ -1,3 +1,1 @@
-// export default "https://harshuh.helioho.st/backend";
-// export default "http://localhost/backend";
-// export default "https://alumni.gbu.ac.in/backend";
+export default "https://alumni.gbu.ac.in/backend";
